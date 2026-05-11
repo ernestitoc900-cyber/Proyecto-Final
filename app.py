92648279adc94dec46f5229d1ce94183d99e3c87
Marvin-IMP <marvin.cruz.imp@gmail.com>
2026-05-11 15:39:48 -0700

    Carpetas y HTML
    agregue la carpeta templates junto con la carpeta static y css, tambien en el file index agregue los primeros botones

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-1
+from flask import Flask, render_template, request
+from entities.pedidoTotal import PedidoTotal
+from entities.producto import Producto
+from entities.subtotalProducto import ProductoPedido
+from enums.tipoProducto import TipoProducto
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
