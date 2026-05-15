16caceba37555a45ab94b2366ad74f3402b13b16
Marvin-IMP <marvin.cruz.imp@gmail.com>
2026-05-14 20:08:39 -0700

    se agrego el link para entrar al menu

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, request
-from entities.pedido_total import PedidoTotal
+from entities.pedido_total import Pedido_total
 from entities.producto import Producto
 from entities.subtotal_producto import ProductoPedido
-from enums.tipo_producto import TipoProducto
+from enums.tipo_producto import Tipo_producto
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
 
 if __name__ == '__main__':
     app.run()
